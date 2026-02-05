114faf1393bbf73bcba8e3247637651f39361b31
gulyaeve <gulyaeve@icloud.com>
2026-02-05 21:04:54 +0300

    fixed alembic

--- app/config.py
+++ app/config.py
@@ -7,28 +7,31 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    MODE: Literal["DEV", "TEST", "PROD"]
-    LOG_LEVEL: str
+    # MODE: Literal["DEV", "TEST", "PROD"]
+    # LOG_LEVEL: str
 
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASS: str
+    POSTGRES_DB: str
 
     @property
-    def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    def database_url(self):
+        user = f"{self.POSTGRES_USER}:{self.POSTGRES_PASS}"
+        database = f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+        return f"postgresql+asyncpg://{user}@{database}"
     
-    TEST_DB_HOST: str
-    TEST_DB_PORT: int
-    TEST_DB_USER: str
-    TEST_DB_PASS: str
-    TEST_DB_NAME: str
-
-    @property
-    def TEST_DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+    # TEST_DB_HOST: str
+    # TEST_DB_PORT: int
+    # TEST_DB_USER: str
+    # TEST_DB_PASS: str
+    # TEST_DB_NAME: str
+    #
+    # @property
+    # def TEST_DATABASE_URL(self):
+    #     return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
     # SMTP_HOST: str
     # SMTP_PORT: int
--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import DeclarativeBase
 from app.config import settings
 
 
-engine = create_async_engine(settings.DATABASE_URL)
+engine = create_async_engine(settings.database_url)
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
