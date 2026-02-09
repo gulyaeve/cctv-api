a87fafee03b31a6f24daf29bc55d1a2b566207a5
Eugene Gulyaev <gulyaeve@vk.com>
2026-02-09 17:38:31 +0300

    Added router for pages

---
+++ app/pages/router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from app.classrooms.models import ClassroomModel
+from app.classrooms.router import get_all_classrooms
+
+
+router = APIRouter(
+    prefix="/pages",
+    tags=["Фронтенд"]
+)
+
+templates = Jinja2Templates(
+    directory="web/templates"
+)
+
+
+@router.get("/monitoring", response_class=HTMLResponse)
+async def get_monitoring_page(
+    request: Request,
+    classroom: ClassroomModel=Depends(get_all_classrooms),
+    ):
+    return templates.TemplateResponse(
+        request=request,
+        name="monitoring/monitoring.html",
+        context={"classroom": classroom}
+        )
