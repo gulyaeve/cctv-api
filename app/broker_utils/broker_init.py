4d075a52d47c5cadd7fe11c3050e8a7d1ab60e7d
Eugene Gulyaev <gulyaeve@vk.com>
2026-04-03 15:57:10 +0300

    Removed rabbitmq old data

--- app/broker_utils/broker_init.py
+++ app/broker_utils/broker_init.py
@@ -4,8 +4,8 @@ from app.config import settings, broker
 
 
 async def declare_exchange_and_queue():
-    queue = RabbitQueue(settings.QUEUE_NAME, auto_delete=True)
-    exchange = RabbitExchange(settings.EXCHANGE_NAME, ExchangeType.FANOUT)
+    # queue = RabbitQueue(settings.QUEUE_NAME, auto_delete=True)
+    # exchange = RabbitExchange(settings.EXCHANGE_NAME, ExchangeType.FANOUT)
 
     queue_add_camera = RabbitQueue("add_camera", auto_delete=False, routing_key="camera.add.*")
     queue_remove_camera = RabbitQueue("remove_camera", auto_delete=False, routing_key="camera.remove.*")
@@ -14,16 +14,16 @@ async def declare_exchange_and_queue():
     camera_exchange = RabbitExchange(settings.CAMERA_EXCHANGE_NAME, ExchangeType.TOPIC, auto_delete=False)
 
     async with broker:
-        tg_queue: aio_pika.RobustQueue = await broker.declare_queue(queue)
-        tg_exchange: aio_pika.RobustExchange = await broker.declare_exchange(exchange)
+        # tg_queue: aio_pika.RobustQueue = await broker.declare_queue(queue)
+        # tg_exchange: aio_pika.RobustExchange = await broker.declare_exchange(exchange)
 
-        await tg_queue.bind(exchange=tg_exchange)
+        # await tg_queue.bind(exchange=tg_exchange)
 
         camera_add_queue: aio_pika.RobustQueue = await broker.declare_queue(queue_add_camera)
         camera_remove_queue: aio_pika.RobustQueue = await broker.declare_queue(queue_remove_camera)
         camera_update_queue: aio_pika.RobustQueue = await broker.declare_queue(queue_update_camera)
 
-        camera_exchange: aio_pika.RobustExchange = await broker.declare_exchange(exchange)
+        # camera_exchange: aio_pika.RobustExchange = await broker.declare_exchange(exchange)
 
         await camera_add_queue.bind(exchange=camera_exchange)
         await camera_remove_queue.bind(exchange=camera_exchange)
