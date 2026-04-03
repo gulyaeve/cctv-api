dfce461b6e4ac87982fba2026a605b8b56987115
Eugene Gulyaev <gulyaeve@vk.com>
2026-04-03 17:33:28 +0300

    change exchange name

--- app/broker_utils/incident_tg.py
+++ app/broker_utils/incident_tg.py
@@ -5,5 +5,5 @@ async def message_to_tg(data):
     async with broker:
         await broker.publish(
             data,
-            exchange=settings.EXCHANGE_NAME
+            exchange=settings.EXCHANGE_NAME_INPUT
         )
