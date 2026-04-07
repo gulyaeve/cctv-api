363c6ebeaf4b245c591c0eb3959a6ee731df70ea
Eugene Gulyaev <gulyaeve@vk.com>
2026-04-07 14:37:32 +0300

    fixed schedules buildings

--- app/pages/schedule.py
+++ app/pages/schedule.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -43,6 +45,28 @@ async def page_get_schedules_page(
 
 
 @router.get(
+    "/buildings",
+    response_class=HTMLResponse,
+    dependencies=[Depends(permission_required("frontend"))]
+)
+async def page_get_schedules_buildings_page(
+    request: Request,
+    # schedules: ScheduleModel=Depends(get_all_schedules),
+    buildings: List[BuildingModel]=Depends(get_all_buildings),
+    current_user: UserModel = Depends(get_current_user)
+    ):
+    return templates.TemplateResponse(
+        request=request,
+        name="schedules/schedules_buildings.html",
+        context={
+            # "schedules": schedules,
+            "current_user": current_user,
+            "buildings": buildings,
+        }
+    )
+
+
+@router.get(
     "/{id}",
     response_class=HTMLResponse,
     dependencies=[Depends(permission_required("frontend"))]
@@ -64,23 +88,3 @@ async def page_get_schedule_info_page(
     )
 
 
-@router.get(
-    "/buildings",
-    response_class=HTMLResponse,
-    dependencies=[Depends(permission_required("frontend"))]
-)
-async def page_get_schedules_buildings_page(
-    request: Request,
-    # schedules: ScheduleModel=Depends(get_all_schedules),
-    buildings: BuildingModel=Depends(get_all_buildings),
-    current_user: UserModel = Depends(get_current_user)
-    ):
-    return templates.TemplateResponse(
-        request=request,
-        name="schedules/schedules_buildings.html",
-        context={
-            # "schedules": schedules,
-            "current_user": current_user,
-            "buildings": buildings,
-        }
-    )
