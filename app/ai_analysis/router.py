e7a88048e4cbce280780b494566eb760ffd1338b
Eugene Gulyaev <gulyaeve@vk.com>
2026-04-08 16:38:36 +0300

    Added ai_analysis get methods

--- app/ai_analysis/router.py
+++ app/ai_analysis/router.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated, Sequence
+
+from fastapi import APIRouter, Depends, Query
 
 from app.ai_analysis.dao import AiAnalysisDAO
-from app.ai_analysis.schemas import AiAnalysisAddScheme, AiAnalysisScheme
+from app.ai_analysis.schemas import AiAnalysisAddScheme, AiAnalysisScheme, AiAnalysisSearchScheme
 from app.exceptions import ObjectMissingException
 from app.users.auth import auth_bearer_token
 
@@ -12,6 +14,22 @@ router = APIRouter(
     # dependencies=[Depends(auth_bearer_token)]
 )
 
+
+@router.get("", response_model=Sequence[AiAnalysisScheme])
+# @cache(expire=60)
+async def get_all_ai_analysis(filter_query: Annotated[AiAnalysisSearchScheme, Query()]):
+    filter_model = filter_query.model_dump(exclude_unset=True, exclude_defaults=True)
+    return await AiAnalysisDAO.find_all(**filter_model)
+
+
+@router.get("/{id}", response_model=AiAnalysisScheme)
+async def get_ai_analysis(id: int):
+    ai_analysis = await AiAnalysisScheme.find_one_or_none(id=id)
+    if ai_analysis is None:
+        raise ObjectMissingException
+    else:
+        return ai_analysis
+    
 
 @router.post("", response_model=AiAnalysisScheme, dependencies=[Depends(auth_bearer_token)], status_code=201)
 async def add_ai_analysis(data: AiAnalysisAddScheme):
